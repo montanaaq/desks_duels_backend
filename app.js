0ec11d03dbd3eaf070176f90bbdf16ded17509fe
Amir <a.presovsky@list.ru>
2024-11-02 14:04:50 +0300

    changed: url to frontend

--- app.js
+++ app.js
@@ -27,6 +27,6 @@ app.use('/duels', duelsRouter);
 sequelize.sync().then(async () => {
     await initializeSeats(); // Инициализация мест
     app.listen(3000, () => {
-        console.log('Server is running on http://localhost:3000, Press Ctrl+C to quit.');
+        console.log('Server is running on port 3000, Press Ctrl+C to quit.');
     });
 }).catch(error => console.error('Error syncing database:', error));
