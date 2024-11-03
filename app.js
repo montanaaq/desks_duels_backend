5d9a476853c39b0b4a782c4a25df1c16e639ba5e
Amir <a.presovsky@list.ru>
2024-11-04 00:58:32 +0300

    added: socket io server

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const { sequelize } = require('./db');
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const http = require('http');
+const { Server } = require('socket.io');
 
 require('dotenv').config();
 
@@ -11,13 +13,41 @@ const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 const seatsRouter = require('./routes/seats');
 const duelsRouter = require('./routes/duels');
+const authRoutes = require('./routes/authRoutes');
 
 app.use(express.json());
 app.use(cors());
 
-// Импорт и подключение маршрутов
-const authRoutes = require('./routes/authRoutes');
-app.use('/', indexRouter)
+// Настраиваем HTTP и WebSocket серверы
+const server = http.createServer(app);
+const io = new Server(server, {
+    cors: {
+        origin: '*', // Замените * на нужный URL, если нужно ограничить доступ
+        methods: ['GET', 'POST']
+    }
+});
+
+// Отслеживаем подключение, отключение и пинг-понг
+io.on('connection', (socket) => {
+    console.log(`User connected: ${socket.id}`);
+
+    // Пинг-понг для стабильности соединения
+    socket.on('ping', () => {
+        console.log(`Received ping from ${socket.id}`);
+        socket.emit('pong');
+    });
+
+    socket.on('seatOccupied', (updatedSeat) => {
+        io.emit('seatUpdated', updatedSeat); // Отправляем обновление всем пользователям
+        console.log(`Seat updated: ${updatedSeat.id}`); // Логируем обновление
+    });
+
+    socket.on('disconnect', () => {
+        console.log(`User disconnected: ${socket.id}`);
+    });
+});
+
+app.use('/', indexRouter);
 app.use('/auth', authRoutes);
 app.use('/users', usersRouter);
 app.use('/seats', seatsRouter);
@@ -26,7 +56,7 @@ app.use('/duels', duelsRouter);
 // Синхронизация базы данных и запуск сервера
 sequelize.sync().then(async () => {
     await initializeSeats(); // Инициализация мест
-    app.listen(3000, () => {
+    server.listen(3000, () => {
         console.log('Server is running on port 3000, Press Ctrl+C to quit.');
     });
 }).catch(error => console.error('Error syncing database:', error));
